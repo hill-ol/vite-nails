359511e4e1e50317c6692d7a834ed5801d4924c7
Olivia Hill <hill.ol@northeastern.edu>
2025-11-03 09:57:01 -0500

    Adding Footer.tsx, all good

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <p className="footer-brand">Luxe Nails</p>
+                <div className="social-links">
+                    <a href="#" className="social-link">Instagram</a>
+                    <a href="#" className="social-link">Facebook</a>
+                    <a href="#" className="social-link">TikTok</a>
+                </div>
+                <p className="copyright">© 2024 Luxe Nails. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
