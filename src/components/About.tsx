693ab32dc646211afe2a3359535c5d063db3d30d
Olivia Hill <hill.ol@northeastern.edu>
2025-11-03 09:54:56 -0500

    Adding About.tsx, needs editing/personalization

---
+++ src/components/About.tsx
@@ -0,0 +1,15 @@
+export default function About() {
+    return (
+        <section id="about" className="about-section">
+            <div className="about-container">
+                <h2 className="section-title">About Us</h2>
+                <p className="about-text">
+                    Description about Ashrita's passions when it comes to nails!
+                </p>
+                <p className="about-text">
+                    Something else about how much she likes doing nails/backstory.
+                </p>
+            </div>
+        </section>
+    );
+}
