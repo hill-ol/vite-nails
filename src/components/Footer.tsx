23d5d1375982fe4303011ef56813a448228a4255
Olivia Hill <hill.ol@northeastern.edu>
2025-11-04 00:07:11 -0500

    Trying something else with the width

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,12 @@ export default function Footer() {
     return (
         <footer className="footer">
             <div className="footer-container">
-                <p className="footer-brand">Luxe Nails</p>
+                <p className="footer-brand">Ashrita's Nails</p>
                 <div className="social-links">
                     <a href="#" className="social-link">Instagram</a>
                     <a href="#" className="social-link">Facebook</a>
                     <a href="#" className="social-link">TikTok</a>
                 </div>
-                <p className="copyright">© 2024 Luxe Nails. All rights reserved.</p>
             </div>
         </footer>
     );
